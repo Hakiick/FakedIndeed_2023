cdd5c418a03690e1837556aab733b670b9fdc932
Hakiick <Hakiick@users.noreply.github.com>
2026-02-19 21:30:38 +0100

    chore(setup): add .gitignore and .env files

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        'MYSQL_HOST': '127.0.0.1',
-        'MYSQL_PORT': '3306',
-        'MYSQL_DATABASE': 'jobboard',
-        'MYSQL_USER': 'root',
-        'MYSQL_PASSWORD': 'new_password',
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
